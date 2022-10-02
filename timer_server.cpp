0162c969f2a631030078cb1855f9b39ae995c573
aumeinctutw1 <benno.schnabel@googlemail.com>
2022-10-02 19:06:15 +0200

    the server must now be startet with port as an argument, otherwise will exit the program

--- timer_server.cpp
+++ timer_server.cpp
@@ -5,15 +5,23 @@
 
 int main(int argc, const char *argv[]) {
     
-    short port = 2000;
+    short port;
+
+    if (argc != 2) {
+        std::cerr << "now port given" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    // read the port from command line
+    port = atoi(argv[1]);
 
     try {
         boost::asio::io_context io_context;
         Server serverInstance(io_context, port);
         io_context.run();
-    }
-    catch (std::exception &e) {
+    } catch (std::exception &e) {
         std::cerr << "Exception: " << e.what() << "\n";
+        exit(EXIT_FAILURE);
     }
 
     return 0;
