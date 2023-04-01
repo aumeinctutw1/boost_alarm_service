9ecbd954fa2123fe5f3d22755c9cb9947e29034a
aumeinctutw1 <benno.schnabel@googlemail.com>
2023-04-01 10:36:22 +0000

    moved and renamed files

--- client.cpp
+++ client.cpp
@@ -1,94 +1,35 @@
-#include <cstdio>
 #include <iostream>
-#include <memory>
-#include <utility>
 #include <chrono>
-#include <arpa/inet.h>
-#include <netdb.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <unistd.h>
 
-//#define PORT 2000
+
 
 int main(int argc, const char *argv[]) {
+
+    if (argc < 5) {
+        std::cerr << "Error: not enough arguments provided" << std::endl;
+        return 1;
+    }
     
+    /* takes 4 Parameters, request id, seconds to wait, server ip, server port*/
+    uint32_t requestId = atoi(argv[1]); 
     int secs = atoi(argv[2]);
-    uint32_t requestId = htonl(atoi(argv[1])); 
     const char *ip = argv[3];
     int port = atoi(argv[4]);
-    char message[] = "Wake up lovely service";
-    uint32_t cookieSize = htonl(strlen(message));
+
+    std::string message = "Wake up lovely service";
+    uint32_t cookieSize = message.length();
+
+    std::cout << "Sending: " << message << ". with size: " << cookieSize << std::endl;
 
     using namespace std::chrono;
-     
-    // timestamp for wake up, 5 Seconds in the future
     uint64_t sec = duration_cast<seconds>(system_clock::now().time_since_epoch()).count() + secs;
-    std::cout << sec << " seconds since the Epoch\n";
 
-    // convert from 64 bit to 2 32 bit integer
-    uint32_t low = htonl((uint32_t)(sec >> 32));
-    uint32_t high = htonl((uint32_t)sec);
+    try {
+        std::cout << "seconds: " << secs << std::endl;
 
-    // send stuff over tcp socket
-    int sock = 0, valread, client_fd;
-    struct sockaddr_in serv_addr;
-    char buffer[1024];
-
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("\n Socket creation error \n");
-        return -1;
-    }
-  
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(port);
-  
-    // Convert IPv4 and IPv6 addresses from text to binary
-    // form
-    if (inet_pton(AF_INET, ip, &serv_addr.sin_addr) <= 0) {
-        printf("\nInvalid address/ Address not supported \n");
-        return -1;
-    }
-  
-    if ((client_fd = connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))) < 0) {
-        printf("\nConnection Failed \n");
-        return -1;
+    } catch (std::exception &e) {
+        std::cerr << "Exception occurred: " << e.what() << std::endl;
     }
 
-    // use htonl to send the data 
-    send(sock, &requestId, sizeof(uint32_t), 0);
-    send(sock, &low, sizeof(uint32_t), 0);
-    send(sock, &high, sizeof(uint32_t), 0);
-    send(sock, &cookieSize, sizeof(uint32_t), 0);
-    send(sock, message, strlen(message), 0);
-
-    printf("Hello message sent\n");
-
-    requestId += 1;
-    send(sock, &requestId, sizeof(uint32_t), 0);
-    send(sock, &low, sizeof(uint32_t), 0);
-    send(sock, &high, sizeof(uint32_t), 0);
-    send(sock, &cookieSize, sizeof(uint32_t), 0);
-    send(sock, message, strlen(message), 0);
-    
-    printf("Hello message sent again\n");
-
-    // read header - request id
-    valread = read(sock, buffer, 4);
-    printf("read bytes: %d\n", valread);
-    printf("Message: %s\n", buffer);
-
-    // read header - cookie size
-    valread = read(sock, buffer, 4);
-    printf("read bytes: %d\n", valread);
-    printf("Message: %s\n", buffer);
-
-    // read message
-    valread = read(sock, buffer, 22);
-    printf("read bytes: %d\n", valread);
-    printf("Message: %s\n", buffer);
-
-    // closing the connected socket
-    close(client_fd);
     return 0;
 }
